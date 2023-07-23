ec9fa72afc90f701c3a8cacd179a889395954c58
schoti183 <tim.s.t@icloud.com>
2023-07-23 13:59:42 +0200

    added min level sum

--- 426_Tree/main.cpp
+++ 426_Tree/main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <iterator>
 #include <ctime>
@@ -9,7 +10,7 @@ int main() {
 	srand(time(nullptr));
 	Tree test;
 	std::cout << "Generating random numbers: " << std::endl;
-	for (unsigned int i = 0; i < 10; ++i) {
+	for (unsigned int i = 0; i < 5; ++i) {
 		int r = rand() % 100;
 		std::cout << r << "\t"; 
 		test.insert(r);
@@ -23,8 +24,11 @@ int main() {
 
 	int * lvlsums = new int [depth+1];
 	test.lvlsum(lvlsums);
-	for (int i = 0; i < depth+1; ++i) {
+	int min_lvl[2] = {0, 100000};
+	for (int i = 1; i < depth+1; ++i) {
 		std::cout << i << ": " << lvlsums[i] << std::endl;
+		if (lvlsums[i] < min_lvl[1]) {min_lvl[0] = i; min_lvl[1] = lvlsums[i];}
 	}
+	std::cout << "Minimum level sum: " << min_lvl[0] << " (" << min_lvl[1] << ")" << std::endl;
 	return 0;
 }
